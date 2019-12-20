fca88bd0706bfb3663c0a7e53ef6ca3bff9e5eb7
natacharome <natacha-rome@hotmail.fr>
2019-12-20 11:03:42 +0100

    add jwt

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -11,7 +11,7 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -23,12 +23,11 @@ export class NavComponent implements OnInit {
     });
   }
 
-  loggedIn(){
-    const token = localStorage.getItem('token');
-    return !!token;
+  loggedIn() {
+    return this.authService.loggedIn();
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
   }
