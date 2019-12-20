85200e304be4079f2bdefcafd33ac5cdeec22396
natacharome <natacha-rome@hotmail.fr>
2019-12-20 10:41:11 +0100

    add Alertify

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -12,16 +13,16 @@ export class RegisterComponent implements OnInit {
   @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register(){
    this.authService.register(this.model).subscribe(() => {
-     console.log('registration successfull');
+      this.alertify.success('registration successfull');
    }, error => {
-     console.log(error);
+      this.alertify.error(error);
    });
   }
   cancel(){
